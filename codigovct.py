d2c3a39651422456241b7a2fe8c47f84e4ab434f
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-19 15:49:25 -0300

    Update codigovct.py

--- codigovct.py
+++ codigovct.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-
 df = pd.read_csv("valorant champions istanbul.csv")
 
 st.title("Buscador de Datos de Jugadores")
@@ -9,29 +8,30 @@ st.title("Buscador de Datos de Jugadores")
 keyword = st.text_input("Ingrese la palabra clave:", "")
 
 keywords_mapping = {
-"jugador": "player",
-"equipo": "team",
-"nacionalidad": "nationality",
-"asesinatos": "kill",
-"muertes": "death",
-"K/D": "rendimiento",
-"KAST": "Impacto por ronda",
-"Prize": "ganancias",
-"Role": "rol",
-"HS %": "porcentaje de headshots",
-"Rounds Played": "rondas jugadas",
-"Rounds Win": "rondas ganadas",
-"Rounds Lose": "rondas perdidas",
-"Rank": "posición por equipos"
+    "jugador": "player",
+    "equipo": "team",
+    "nacionalidad": "nationality",
+    "asesinatos": "kill",
+    "muertes": "death",
+    "K/D": "rendimiento",
+    "KAST": "Impacto por ronda",
+    "Prize": "ganancias",
+    "Role": "rol",
+    "HS %": "porcentaje de headshots",
+    "Rounds Played": "rondas jugadas",
+    "Rounds Win": "rondas ganadas",
+    "Rounds Lose": "rondas perdidas",
+    "Rank": "posición por equipos"
 }
-if keyword in keywords_mapping:
-column_name = keywords_mapping[keyword]
 
-filtered_data = df[df[column_name].astype(str).str.contains(keyword, case=False, na=False)]
-if not filtered_data.empty:
-st.write("Resultados encontrados:")
-st.dataframe(filtered_data)
+if keyword in keywords_mapping:
+    column_name = keywords_mapping[keyword]  # Esta línea debe estar indentada
+
+    filtered_data = df[df[column_name].astype(str).str.contains(keyword, case=False, na=False)]
+    if not filtered_data.empty:
+        st.write("Resultados encontrados:")
+        st.dataframe(filtered_data)
+    else:
+        st.write("No se encontraron resultados.")
 else:
-st.write("No se encontraron resultados.")
-else:
-st.write("La palabra clave no es válida.")
+    st.write("La palabra clave no es válida.")
