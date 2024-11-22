a3c5193bf26720b16a2eb563c15e89254f67e722
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-22 12:27:59 -0300

    Update codigovct.py

--- codigovct.py
+++ codigovct.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-from PIL import Image
 
 # Cargar el archivo CSV
 df = pd.read_csv("valorant champions istanbul.csv")
@@ -36,11 +35,28 @@ def mejor_rendimiento_por_equipo():
     filas_seleccionadas = df.iloc[[3,5,10,17,22,25,32,35]]  
     st.dataframe(filas_seleccionadas)
 
-# Función para redimensionar la imagen
-def resize_image(image_path, width, height):
-    img = Image.open(image_path)
-    img = img.resize((width, height))  # Redimensiona la imagen
-    return img
+# Establece el fondo de la página con la imagen "Fondo.png"
+st.markdown(
+    f"""
+    <style>
+        .reportview-container {{
+            background-image: url({image_path_fondo});
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            height: 100vh;
+            color: white;
+        }}
+        .stButton>button {{
+            background-color: rgba(0, 0, 0, 0.5);
+            color: white;
+            font-size: 20px;
+            border-radius: 8px;
+        }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 # Título de la página
 st.title("Datos que creemos te gustarán saber")
@@ -63,56 +79,39 @@ if st.session_state.page == "home":
 # Página para mostrar el "mejor rendimiento"
 elif st.session_state.page == "mejor_rendimiento":
     mejor_rendimiento()
-    # Redimensionar la imagen y mostrarla
-    st.tittle("Jugador con mejor rendimiento del torneo")
-    img_yay = resize_image(image_yay, 240, 288)
-    st.image(img_yay, caption="")
+    # Mostrar las dos imágenes "Fondo.png" y "duelistas.jpg"
+    st.image(image_yay, use_column_width=True, caption="", width=200)
     if st.button("Volver a la página principal"):
         st.session_state.page = "home"
 
 # Página para mostrar el "peor rendimiento"
 elif st.session_state.page == "peor_rendimiento":
     peor_rendimiento()
-    # Redimensionar la imagen y mostrarla
-    st.tittle("Jugador con peor rendimiento del torneo")
-    img_ange1 = resize_image(image_ANGE1, 240, 288)
-    st.image(img_ange1, caption="Presentación ANGE1")
+    # Mostrar las dos imágenes "Fondo.png" y "duelistas.jpg"
+    st.image(image_ANGE1, use_column_width=True, caption="Presentación ANGE1")
     if st.button("Volver a la página principal"):
         st.session_state.page = "home"
 
 # Página para mostrar el "jugador con más kills"
 elif st.session_state.page == "mas_kills":
     mas_kills()
-    # Redimensionar la imagen y mostrarla
-    st.tittle("Jugador conmás kills del torneo")
-    img_yay = resize_image(image_yay, 240, 288)
-    st.image(img_yay, caption="Presentación yay")
+    # Mostrar las dos imágenes "Fondo.png" y "duelistas.jpg"
+    st.image(image_yay, use_column_width=True, caption="Presentación yay")
     if st.button("Volver a la página principal"):
         st.session_state.page = "home"
 
 # Página para mostrar el "mejor rendimiento por equipo"
 elif st.session_state.page == "mejor_rendimiento_por_equipo":
     mejor_rendimiento_por_equipo()
-    # Redimensionar las imágenes y mostrarlas
-    st.tittle("Jugadores con mejor rendimiento del torneo por equipo")
-    img_Less = resize_image(image_Less, 240, 288)
-    img_yay = resize_image(image_yay, 240, 288)
-    img_MaKo = resize_image(image_MaKo, 240, 288)
-    img_suygetsu = resize_image(image_suygetsu, 240, 288)
-    img_Cryocells = resize_image(image_Cryocells, 240, 288)
-    img_Derke = resize_image(image_Derke, 240, 288)
-    img_Scream = resize_image(image_Scream, 240, 288)
-    img_kiNgg = resize_image(image_kiNgg, 240, 288)
-
-    # Mostrar las imágenes redimensionadas
-    st.image(img_Less, caption="Presentación Less")
-    st.image(img_yay, caption="Presentación yay")
-    st.image(img_MaKo, caption="Presentación MaKo")
-    st.image(img_suygetsu, caption="Presentación suygetsu")
-    st.image(img_Cryocells, caption="Presentación Cryocells")
-    st.image(img_Derke, caption="Presentación Derke")
-    st.image(img_Scream, caption="Presentación Scream")
-    st.image(img_kiNgg, caption="Presentación kiNgg")
+    # Mostrar las dos imágenes "Fondo.png" y "duelistas.jpg"
+    st.image(image_Less, use_column_width=True, caption="Presentación ")
+    st.image(image_yay, use_column_width=True, caption="Presentación ")
+    st.image(image_MaKo, use_column_width=True, caption="Presentación ")
+    st.image(image_suygetsu, use_column_width=True, caption="Presentación ")
+    st.image(image_Cryocells, use_column_width=True, caption="Presentación ")
+    st.image(image_Derke, use_column_width=True, caption="Presentación ")
+    st.image(image_Scream, use_column_width=True, caption="Presentación ")
+    st.image(image_kiNgg, use_column_width=True, caption="Presentación ")
 
     if st.button("Volver a la página principal"):
         st.session_state.page = "home"
