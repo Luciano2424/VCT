9db27f63fc7a845e862a289fe1c6660306aba44b
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-23 14:40:05 -0300

    Update codigovct.py

--- codigovct.py
+++ codigovct.py
@@ -4,17 +4,10 @@ from PIL import Image
 
 df = pd.read_csv("valorant champions istanbul.csv")
 
-image_path_fondo = "Fondo.png"
-image_path_duelistas = "duelistas.jpg"
 image_yay = "yay.jpeg"
-image_Cryocells = "Cryocells.jpg"
 image_ANGE1 = "ANGE1.jpg"
-image_Derke = "Derke.jpg"
 image_MaKo = "MaKo.jpg"
-image_Scream = "Scream.jpg"
-image_kiNgg = "kiNgg.jpg"
 image_suygetsu = "suygetsu.jpg"
-image_Less = "Less.jpeg"
 
 
 def mejor_rendimiento():
@@ -33,9 +26,6 @@ def mejor_rendimiento_por_equipo():
     filas_seleccionadas = df.iloc[[3,5,10,17,22,25,32,35]]  
     st.dataframe(filas_seleccionadas)
 
-if st.session_state.page == "home":  
-    st.title("Datos que creemos te gustarán saber")
-
 if "page" not in st.session_state:
     st.session_state.page = "home"
 
@@ -47,21 +37,26 @@ def resize_image(image_path, width=130, height=152):
 # Función para agregar leyenda con tipografía personalizada
 def display_image_with_caption(image_path, caption):
     img_resized = resize_image(image_path)
-    st.image(img_resized)
-    st.markdown(
-        f"""
-        <style>
-        .caption {{
-            font-family: 'Arial Black', sans-serif;
-            font-size: 13px;
-            font-weight: bold;
-        }}
-        </style>
-        <p class="caption">{caption}</p>
-        """, unsafe_allow_html=True
-    )
+    col = st.columns(1)  # Usamos 1 columna para centrar la imagen
+    with col[0]:
+        st.image(img_resized)
+        st.markdown(
+            f"""
+            <style>
+            .caption {{
+                font-family: 'Arial Black', sans-serif;
+                font-size: 18px;
+                font-weight: bold;
+                text-align: center;
+            }}
+            </style>
+            <p class="caption">{caption}</p>
+            """, unsafe_allow_html=True
+        )
 
 if st.session_state.page == "home":
+    st.title("Datos que creemos te gustarán saber")
+    
     if st.button("Cuál fue el jugador con mejor rendimiento global del torneo"):
         st.session_state.page = "mejor_rendimiento"
     if st.button("Cuál fue el jugador con peor rendimiento global del torneo"):
@@ -99,7 +94,7 @@ elif st.session_state.page == "mejor_rendimiento_por_equipo":
     # Usando las columnas para mostrar las imágenes
     col1, col2, col3, col4 = st.columns(4)  
     with col1:
-        display_image_with_caption(image_Less, "Less Top 1")
+        display_image_with_caption(image_MaKo, "MaKo Top 1")
 
     with col2:
         display_image_with_caption(image_yay, "yay Top 2")
@@ -109,19 +104,6 @@ elif st.session_state.page == "mejor_rendimiento_por_equipo":
 
     with col4:
         display_image_with_caption(image_suygetsu, "suygetsu Top 4")
-
-    col5, col6, col7, col8 = st.columns(4)
-    with col5:
-        display_image_with_caption(image_Cryocells, "Cryocells Top 5")
-
-    with col6:
-        display_image_with_caption(image_Derke, "Derke Top 6")
-
-    with col7:
-        display_image_with_caption(image_Scream, "Scream Top 7")
-
-    with col8:
-        display_image_with_caption(image_kiNgg, "kiNgg Top 8")
 
     if st.button("Volver a la página principal"):
         st.session_state.page = "home"
