798d38f7c9d8a33154da782509c918a0a86aba50
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-23 14:48:43 -0300

    Update codigovct.py

--- codigovct.py
+++ codigovct.py
@@ -4,10 +4,17 @@ from PIL import Image
 
 df = pd.read_csv("valorant champions istanbul.csv")
 
+image_path_fondo = "Fondo.png"
+image_path_duelistas = "duelistas.jpg"
 image_yay = "yay.jpeg"
+image_Cryocells = "Cryocells.jpg"
 image_ANGE1 = "ANGE1.jpg"
+image_Derke = "Derke.jpg"
 image_MaKo = "MaKo.jpg"
+image_Scream = "Scream.jpg"
+image_kiNgg = "kiNgg.jpg"
 image_suygetsu = "suygetsu.jpg"
+image_Less = "Less.jpeg"
 
 
 def mejor_rendimiento():
@@ -26,6 +33,9 @@ def mejor_rendimiento_por_equipo():
     filas_seleccionadas = df.iloc[[3,5,10,17,22,25,32,35]]  
     st.dataframe(filas_seleccionadas)
 
+if st.session_state.page == "home":  
+    st.title("Datos que creemos te gustarán saber")
+
 if "page" not in st.session_state:
     st.session_state.page = "home"
 
@@ -34,29 +44,25 @@ def resize_image(image_path, width=130, height=152):
     img_resized = img.resize((width, height))  
     return img_resized
 
-# Función para mostrar imagen y leyenda centrados
+# Función para agregar leyenda con tipografía personalizada
 def display_image_with_caption(image_path, caption):
     img_resized = resize_image(image_path)
-    col = st.columns(1)  # Usamos 1 columna para centrar la imagen
-    with col[0]:
-        st.image(img_resized)
-        st.markdown(
-            f"""
-            <style>
-            .caption {{
-                font-family: 'Arial Black', sans-serif;
-                font-size: 18px;
-                font-weight: bold;
-                text-align: center;
-            }}
-            </style>
-            <p class="caption">{caption}</p>
-            """, unsafe_allow_html=True
-        )
+    st.image(img_resized)
+    st.markdown(
+        f"""
+        <style>
+        .caption {{
+            font-family: 'Arial Black', sans-serif;
+            font-size: 18px;
+            font-weight: bold;
+            text-align: center;
+        }}
+        </style>
+        <p class="caption">{caption}</p>
+        """, unsafe_allow_html=True
+    )
 
 if st.session_state.page == "home":
-    st.title("Datos que creemos te gustarán saber")
-    
     if st.button("Cuál fue el jugador con mejor rendimiento global del torneo"):
         st.session_state.page = "mejor_rendimiento"
     if st.button("Cuál fue el jugador con peor rendimiento global del torneo"):
@@ -74,7 +80,7 @@ elif st.session_state.page == "mejor_rendimiento":
         st.session_state.page = "home"
 
 elif st.session_state.page == "peor_rendimiento":
-    st.title("Jugador con rendimiento más bajo")
+    st.title("Jugador con rendimiento mas bajo")
     peor_rendimiento()
     display_image_with_caption(image_ANGE1, "Presentación ANGE1")
     if st.button("Volver a la página principal"):
@@ -83,11 +89,7 @@ elif st.session_state.page == "peor_rendimiento":
 elif st.session_state.page == "mas_kills":
     st.title("Jugador con más bajas")
     mas_kills()
-    col1, col2 = st.columns(2)
-        with col1:
-            display_image_with_caption(image_MaKo, "Presentación yay")
-        with col2:
-            display_image_with_caption(image_br, "Nacionalidad")
+    display_image_with_caption(image_yay, "Presentación yay")
     if st.button("Volver a la página principal"):
         st.session_state.page = "home"
 
@@ -98,13 +100,33 @@ elif st.session_state.page == "mejor_rendimiento_por_equipo":
     # Usando las columnas para mostrar las imágenes
     col1, col2, col3, col4 = st.columns(4)  
     with col1:
-        display_image_with_caption(image_MaKo, "MaKo Top 1")
+        display_image_with_caption(image_Less, "Less Top 1")
 
     with col2:
         display_image_with_caption(image_yay, "yay Top 2")
 
     with col3:
         display_image_with_caption(image_MaKo, "MaKo Top 3")
+
+    with col4:
+        display_image_with_caption(image_suygetsu, "suygetsu Top 4")
+
+    col5, col6, col7, col8 = st.columns(4)
+    with col5:
+        display_image_with_caption(image_Cryocells, "Cryocells Top 5")
+
+    with col6:
+        display_image_with_caption(image_Derke, "Derke Top 6")
+
+    with col7:
+        display_image_with_caption(image_Scream, "Scream Top 7")
+
+    with col8:
+        display_image_with_caption(image_kiNgg, "kiNgg Top 8")
+
+    if st.button("Volver a la página principal"):
+        st.session_state.page = "home"
+
 
     with col4:
         display_image_with_caption(image_suygetsu, "suygetsu Top 4")
