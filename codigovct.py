f8aa2cf7e83bc5c0696fa75731b25ecb26086d88
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-24 01:02:49 -0300

    Update codigovct.py

--- codigovct.py
+++ codigovct.py
@@ -3,10 +3,8 @@ import pandas as pd
 from PIL import Image  
 import matplotlib.pyplot as plt
 
-# Cargar datos
 df = pd.read_csv("valorant champions istanbul.csv")
 
-# Rutas de las imágenes
 image_yay = "yay.jpeg"
 image_Cryocells = "Cryocells.jpg"
 image_ANGE1 = "ANGE1.jpg"
@@ -17,7 +15,6 @@ image_kiNgg = "kiNgg.jpg"
 image_suygetsu = "suygetsu.jpg"
 image_Less = "Less.jpeg"
 
-# Funciones para mostrar datos
 def mejor_rendimiento():
     filas_seleccionadas = df.iloc[[5]]  
     st.dataframe(filas_seleccionadas)
@@ -34,20 +31,18 @@ def mejor_rendimiento_por_equipo():
     filas_seleccionadas = df.iloc[[3, 5, 10, 17, 22, 25, 32, 35]]  
     st.dataframe(filas_seleccionadas)
 
-# Asegurarse de que "page" esté en el estado de la sesión
 if "page" not in st.session_state:
     st.session_state.page = "home"
 
-# Función para redimensionar imágenes
 def resize_image(image_path, width=130, height=152):
     img = Image.open(image_path)  
     img_resized = img.resize((width, height))  
     return img_resized
 
-# Función para mostrar imágenes con leyenda
 def display_image_with_caption(image_path, caption):
     img_resized = resize_image(image_path)
     st.image(img_resized)
+    
     st.markdown(
         f"""
         <style>
@@ -61,11 +56,9 @@ def display_image_with_caption(image_path, caption):
         """, unsafe_allow_html=True
     )
 
-# Página de inicio (home)
 if st.session_state.page == "home":  
     st.title("Análisis y Estadísticas del VCT Masters Reykjavik 2022: ¡Revive la Emoción del Torneo!")
     
-    # URL del video de YouTube
     video_presentación_ = "https://www.youtube.com/watch?v=j2Z4qYJ3Jtc&ab_channel=VALORANTChampionsTour"
     st.video(video_presentación_)
 
@@ -73,8 +66,55 @@ if st.session_state.page == "home":
 
     with col2:
         st.subheader("Presentación de los equipos participantes del toreno")
-        
-    # Selección de página
+
+def imagenes_logos(image_path, width=200, height=200):
+    try:
+        img = Image.open(image_path)
+        img_resized = img.resize((width, height))
+        return img_resized
+    except Exception as e:
+        st.error(f"Error al cargar la imagen {image_path}: {e}")
+        return None
+
+image_LEV = "LEV.jpg"
+image_DRX = "DRX.jpg"
+image_XSET = "XSET.jpg"
+image_FNC = "FNC.jpg"
+image_FPX = "FPX.jpg"
+image_OPTC = "OPTC.jpg"
+image_TL = "TL.jpg"
+image_LOUD = "LOUD.jpg"
+
+# Función para mostrar la imagen con su nombre
+def display_logos(image_path, name, width=100, height=100):
+    img_resized = imagenes_logos(image_path, width, height)
+    if img_resized:
+        st.image(img_resized)
+        st.caption(name)  
+
+col1, col2, col3 = st.columns([1, 1, 1]) 
+with col2:
+    display_logos(image_LOUD, "   1 LOUD")
+
+col4, col5, col6 = st.columns([1, 1, 1])  
+with col4:
+    display_logos(image_OPTC, "2 OPTC")
+with col6:
+    display_logos(image_DRX, "3 DRX")
+
+col7, col8, col9, col10, col11 = st.columns([1, 1, 1, 1, 1])  
+with col7:
+    display_logos(image_FPX, "4 FPX")
+with col8:
+    display_logos(image_XSET, "5 XSET")
+with col9:
+    display_logos(image_FNC, "6 FNC")
+with col10:
+    display_logos(image_TL, "7 TeamLiquid")
+with col11:
+    display_logos(image_LEV, "8 Leviatán")
+
+    
     page_selection = st.selectbox(
         "Datos que creemos te gustarán saber",
         ["Ház click para desplegar las opciones", 
@@ -93,10 +133,6 @@ if st.session_state.page == "home":
     elif page_selection == "Cuáles fueron los jugadores con mejor rendimiento de cada equipo?":
         st.session_state.page = "mejor_rendimiento_por_equipo"
 
-    # Sección de gráficos (Mejores momentos del torneo)
-    st.subheader("Mejores momentos del torneo")
-
-    # Gráfico de KD/Rendimiento
     st.subheader("¡Descubre el Poder de los Equipos!")
     st.text("Este gráfico muestra cómo se desempeñan los equipos en cuanto a su ratio de Kills/Deaths (K/D). Los equipos con el mejor desempeño suelen tener una mayor proporción de muertes por baja, lo que refleja una ejecución más eficiente en el juego. ¡Ve quién lidera el torneo en rendimiento!")
     plt.figure(figsize=(10, 6))
@@ -106,9 +142,8 @@ if st.session_state.page == "home":
     plt.ylabel('Promedio K/D')
     plt.title('K/D promedio por equipo')
     plt.xticks(rotation="horizontal", ha='right')
-    st.pyplot()  # Muestra el gráfico en Streamlit
+    st.pyplot()  
 
-    # Gráfico de kills
     st.subheader("¿Quién es el Rey de las Bajas?")
     st.text("Este gráfico destaca la acumulación de kills de cada jugador a lo largo del torneo. Si estás buscando al jugador con más acción en el campo de batalla, aquí puedes ver quién se lleva la corona de las eliminaciones. ¡El jugador más letal está aquí!")
     plt.figure(figsize=(15, 6))
@@ -118,9 +153,8 @@ if st.session_state.page == "home":
     plt.ylabel('Kills')
     plt.title('Jugador con más kills')
     plt.xticks(rotation=45, ha='right')
-    st.pyplot()  # Muestra el gráfico en Streamlit
+    st.pyplot()  
 
-    # Gráfico de Muertes
     st.subheader("¡Las Muertes también Hablan!")
     st.text("A veces el precio del juego es alto, y este gráfico la cantidad de muertes de cada jugador. Aunque no es lo más positivo, saber quién lidera en esta categoría puede dar pistas sobre el estilo de juego o los desafíos a los que se enfrentan los jugadores. ¡Entérate de quiénes son los más golpeados en el torneo!")
     plt.figure(figsize=(15, 6))
@@ -130,9 +164,8 @@ if st.session_state.page == "home":
     plt.ylabel('Muertes')
     plt.title('Jugador con más muertes')
     plt.xticks(rotation=45, ha='right')
-    st.pyplot()  # Muestra el gráfico en Streamlit
+    st.pyplot()  
 
-    # Gráfico de Victorias por equipo
     st.subheader("Equipos Triunfadores: ¿Quién Tiene la Mayor Cantidad de Victorias?")
     st.text("El éxito no solo se mide en kills, sino también en victorias. Este gráfico revela a los equipos que más veces han salido victoriosos durante el torneo, mostrando quién tiene el control total en el campo de juego. ¡Descubre al equipo imbatible de esta edición!")
     plt.figure(figsize=(15, 6))
@@ -142,9 +175,8 @@ if st.session_state.page == "home":
     plt.ylabel('Victorias')
     plt.title('Equipo con más victorias')
     plt.xticks(rotation="horizontal", ha="center")
-    st.pyplot()  # Muestra el gráfico en Streamlit
+    st.pyplot()  
 
-    # Gráfico de Derrotas por equipo
     st.subheader("¿Quién Sufrió Más Derrotas?")
     st.text("Aunque cada derrota es una oportunidad para aprender, algunos equipos han tenido más dificultades que otros. Este gráfico muestra a los equipos con más derrotas, lo que podría reflejar puntos débiles o dificultades para adaptarse al estilo del torneo. ¡Descubre qué equipos tuvieron más trabajo durante la competencia!")
     plt.figure(figsize=(15, 6))
@@ -154,9 +186,8 @@ if st.session_state.page == "home":
     plt.ylabel('Derrotas')
     plt.title('Equipo con más derrotas')
     plt.xticks(rotation="horizontal", ha="center")
-    st.pyplot()  # Muestra el gráfico en Streamlit
+    st.pyplot()  
 
-# Páginas adicionales
 elif st.session_state.page == "mejor_rendimiento":
     st.title("Jugador con mejor rendimiento")
     mejor_rendimiento()
