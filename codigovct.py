d927220f79217a1c3e70bb3877c45a6766d8cdc7
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-23 21:09:28 -0300

    Update codigovct.py

--- codigovct.py
+++ codigovct.py
@@ -63,8 +63,7 @@ if st.session_state.page == "home":
     st.title("Datos que creemos te gustarán saber")
 
     # Replace buttons with selectbox
-    page_selection = st.selectbox(
-        "Selecciona una opción:",
+    page_selection = st.selectbox(,
         ["Ház click para desplegar las opciones", "Cuál fue el jugador con mejor rendimiento global del torneo", 
          "Cuál fue el jugador con peor rendimiento global del torneo", 
          "Cuál fue el jugador con más kills?", 
@@ -135,3 +134,5 @@ elif st.session_state.page == "mejor_rendimiento_por_equipo":
 
     if st.button("Volver a la página principal"):
         st.session_state.page = "home"
+
+#Graficos
